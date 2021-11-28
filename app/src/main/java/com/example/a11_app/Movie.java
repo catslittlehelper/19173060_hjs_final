3cbeb63e6af0283ff22710eb8c8db891a42e226e
User <catslittlehelper@naver.com>
2021-11-28 16:31:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a11_app/Movie.java
@@ -0,0 +1,19 @@
+package com.example.a11_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAmt;
+    String salesShare;
+    String salesInten;
+    String salesChance;
+    String salesAcc;
+    String audiCnt;
+    String scrnCnt;
+    String showCnt;
+}

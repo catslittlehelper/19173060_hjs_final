daacb912957d737eec070a2f01ca1f11e0a29c36
User <catslittlehelper@naver.com>
2021-12-09 18:12:21 +0900

    기말고사 졸업작품

--- app/src/main/java/com/example/a11_app/MainActivity.java
+++ app/src/main/java/com/example/a11_app/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.a11_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,16 +28,22 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     TextView textView;
+    DatePickerDialog.OnDateSetListener callbackMethon;
 
     static RequestQueue requestQueue;
+
+    RecyclerView recyclerView;
+    MovieAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.InitalizeListnner();
+
         editText = findViewById(R.id.xeditText);
-        textView = findViewById(R.id.xtextView);
+        textView = findViewById(R.id.textView);
 
         Button button = findViewById(R.id.xbutton);
         button.setOnClickListener(new View.OnClickListener() {
@@ -41,9 +52,42 @@ public class MainActivity extends AppCompatActivity {
                 makeRequest();
             }
         });
+
+        Button dateButton = findViewById(R.id.dataButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateProcess(v);
+            }
+        });
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new MovieAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+    public void InitalizeListnner(){
+        callbackMethon = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String year1 = String.valueOf(year);
+                String month1 = String.valueOf(month);
+                String dayOfMonth1 = String.valueOf(dayOfMonth);
+                String date = year1 + month1 + dayOfMonth1;
+                editText.setText("https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=b151453efefc24a3ed2fb130d19c4299&targetDt=" + date);
+
+            }
+        };
+    }
+    public void dateProcess(View v)
+    {
+        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethon,2021,11,16);
+        dialog.show();
     }
     public void makeRequest() {
         String url = editText.getText().toString();
@@ -80,11 +124,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void println(String data) {
-        textView.append(data + "\n");
+        Log.d("MainActivity", data);
     }
     public void processResponse(String response){
         Gson gson = new Gson();
         MovieList movieList = gson.fromJson(response, MovieList.class);
         println("영화 정보의 수 : " + movieList.boxOfficeResult.dailyBoxOfficeList.size());
+
+        for (int i=0; i<movieList.boxOfficeResult.dailyBoxOfficeList.size();i++){
+            Movie movie = movieList.boxOfficeResult.dailyBoxOfficeList.get(i);
+            adapter.addItem(movie);
+        }
+        adapter.notifyDataSetChanged();
     }
 }
